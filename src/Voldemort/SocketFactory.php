f86fe0e889fe2d40c6f73a7c1c479b143bb83d10
manavo <manavo@gmail.com>
2014-07-01 15:50:23 +0100

    Override to create our own socket

--- src/Voldemort/SocketFactory.php
+++ src/Voldemort/SocketFactory.php
@@ -4,4 +4,14 @@ namespace Voldemort;
 
 class SocketFactory extends \Socket\Raw\Factory
 {
+
+    public function create($domain, $type, $protocol)
+    {
+        $sock = @socket_create($domain, $type, $protocol);
+        if ($sock === false) {
+            throw Exception::createFromGlobalSocketOperation('Unable to create socket');
+        }
+        return new Socket($sock);
+    }
+
 }
